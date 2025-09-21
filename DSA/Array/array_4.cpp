8a7a55d5d27e23d20f5c06b4b778365edb78d0c1
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-21 18:49:05 +0530

    update

--- DSA/Array/array_4.cpp
+++ DSA/Array/array_4.cpp
@@ -7,7 +7,7 @@ bool isSorted(int arr[] ,int n){
   for (int i = 0; i < n; i++) {
     for (int j = i + 1; j < n; j++) {
       if (arr[j] < arr[i])
-        return false;
+        return false
     }
   }
 
