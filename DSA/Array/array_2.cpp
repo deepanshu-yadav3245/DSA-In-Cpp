6f3cb9563fd2267c3b2b8c579f0ff5845d388300
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-21 18:44:34 +0530

    update

--- DSA/Array/array_2.cpp
+++ DSA/Array/array_2.cpp
@@ -8,7 +8,7 @@ int findLargestElement(int arr[],int n ){
     int max = arr[0];
     for(int i = 0; i < n; i++){
         if(max < arr[i] ){
-            max = arr[i];
+            max = arr[i]
         }
     }
     return max;
