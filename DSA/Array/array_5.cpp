d32fc4679c6450b111e4effbc6184111e7ee7eb9
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-22 11:29:13 +0530

    update

--- DSA/Array/array_5.cpp
+++ DSA/Array/array_5.cpp
@@ -7,8 +7,8 @@ int removeDuplicates(int arr[], int n){
     for(int j = 1; j < n; j++){
         if(arr[i] != arr[j]){
             i++;
-            arr[i] = arr[j];
-        }
+            arr[i] = arr[j]
+        
     }
     return i + 1;
 }
