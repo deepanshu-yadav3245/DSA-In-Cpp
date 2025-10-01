9ee05ecd83a1ff1969bbb9200f94c1ae9270bbb2
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-10-01 14:21:07 +0530

    update

--- Basic of C++/loop1.cpp
+++ Basic of C++/loop1.cpp
@@ -24,22 +24,40 @@
 
 
 // Sum Of All Odd Number From 1 to N;
+#include <iostream>
+using namespace std;
 
+int main(){
+    int n = 19;
+    int oddSum = 0;
+
+    int i = 1;
+    while (i <= n){
+        if(i%2!=0){
+            oddSum += i;
+        }
+     i++;
+    }
+    
+    cout<<"The Sum Of Odd Number = "<<oddSum<<endl;
+    return 0;
+
+}
+
+
+ // Sum Of number form 1 to n;
 // #include <iostream>
 // using namespace std;
 
 // int main(){
-//     int n = 8;
-//     int oddSum = 0;
+//     int n = 10;
+//     int sum = 0;
 
-//     for(int i =1; i <= n; i++){
-        
-//         if(i%2!=0){
-//             oddSum += i;
-//         }
+//     for(int i=1; i<=n; i++){
+//         sum = sum + i;   
 //     }
-//     cout<<"The Sum Of Odd Number = "<<oddSum<<endl;
+
+//      cout << "Sum = " <<sum << endl;
+
 //     return 0;
-
 // }
-
