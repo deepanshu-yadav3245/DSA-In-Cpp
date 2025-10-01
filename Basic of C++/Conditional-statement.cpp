81343d3671e6437a87699896485877095f21f537
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-10-01 14:20:31 +0530

    update

--- Basic of C++/Conditional-statement.cpp
+++ Basic of C++/Conditional-statement.cpp
@@ -20,21 +20,21 @@ int main () {
 
 
 
-// (2):- IF..ELSE Statement
-#include <iostream>
-using namespace std;
-int main () {
-	int age;
-	cout << "How old are you? ";
-	cin >> age;
+// // (2):- IF..ELSE Statement
+// #include <iostream>
+// using namespace std;
+// int main () {
+// 	int age;
+// 	cout << "How old are you? ";
+// 	cin >> age;
 
-	if(age > 18) {
-		cout << "You are an adult!";
-	}
-	else {
-		cout << "You are not yet an adult! ";
-	}
-	cout << "Because you are " << age << " years old";
-   return 0;
-}
+// 	if(age > 18) {
+// 		cout << "You are an adult!";
+// 	}
+// 	else {
+// 		cout << "You are not yet an adult! ";
+// 	}
+// 	cout << "Because you are " << age << " years old";
+//    return 0;
+// }
 
