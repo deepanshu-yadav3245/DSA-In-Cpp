ce27d2b7c458a94ba024e64cb289bb3a4d205a36
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-10-02 06:54:30 +0530

    update

--- DSA/Array/array_1.cpp
+++ DSA/Array/array_1.cpp
@@ -1,18 +1,18 @@
 // In C++, an array is a data structure that is used to store multiple values of similar data types in a contiguous memory location..
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main(){
+int main(){
 
-//     int my_array[5] = {2,3,4,5,6};
+    int my_array[5] = {2,3,4,5,6};
 
-//     for (int i = 0; i < 5; i++){
-//         cout << my_array[i] << endl;
+    for (int i = 0; i < 5; i++){
+        cout << my_array[i] << endl;
     
-//     }
-//     return 0 ;
-// };
+    }
+    return 0 ;
+};
 
 
 
