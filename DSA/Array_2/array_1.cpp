57fe8ee573177149b75551198649584d31f7517c
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-10-03 13:26:03 +0530

    update

--- DSA/Array_2/array_1.cpp
+++ DSA/Array_2/array_1.cpp
@@ -1,20 +1,18 @@
-// Linear Search...
+// In C++, an array is a data structure that is used to store multiple values of similar data types in a contiguous memory location..
 
 #include<iostream>
 using namespace std;
 
-int linearSearch(int arr[], int n , int num){
-    for(int i = 0; i < n; i++){
-        if ( arr[i] = num){
-            return arr[i];
-        }
-    }
-    return -1;
-}
 int main(){
-    int n = 7 , num = 5;
-    int arr[n] = {1,2,3,4,5,6,7};
-    cout << linearSearch(arr,n,num) << endl;
 
-    return n;
-}
+    int my_array[5] = {2,3,4,5,6};
+
+    for (int i = 0; i < 5; i++){
+        cout << my_array[i] << endl;
+    
+    }
+    return 0 ;
+};
+
+
+
