5ed0f81296927eac6a746ef29eeca9c98f982854
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-10-04 10:47:15 +0530

    update

--- Functions/function.cpp
+++ Functions/function.cpp
@@ -7,38 +7,38 @@
 //              //code to be executed    
 //        }  
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int sum(int a,int b){ // parameter
-//     int s = a + b;
-//     return s;
-// }
+int sum(int a,int b){ // parameter
+    int s = a + b;
+    return s;
+}
 
-// int main(){
-//     cout<<sum(20,4900); //Argument 
-//     return 0;
-// }
+int main(){
+    cout<<sum(20,4900); //Argument 
+    return 0;
+}
 
 
 
 // Calculate sum of numebrs from 1 to N
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int sumN(int n){
-//     int sum = 0;
+int sumN(int n){
+    int sum = 0;
 
-//     for(int i = 1; i <= n; i++){
-//         sum += i;
-//     }
-//     return sum;
-// }
-// int main(){
-//     cout << sumN (120) << endl ;
-//     cout << sumN (200) << endl ;
-//     return 0;
-// }
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+int main(){
+    cout << sumN (120) << endl ;
+    cout << sumN (200) << endl ;
+    return 0;
+}
 
 // calculate N factorial:--
 
