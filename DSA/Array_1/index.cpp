c958a3f0f0023cff74799a3c479fa975d7264a62
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-10-05 23:57:12 +0530

    update

---
+++ DSA/Array_1/index.cpp
@@ -0,0 +1,13 @@
+
+// find no indexing.
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int arr[5] = {4,7,6,7,8};
+    for(int i = 0; i <n; i++){
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
