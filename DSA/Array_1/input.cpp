0c2a55bf2cd2e27fe9bf277783fd362e946823f9
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-10-05 23:57:28 +0530

    update

---
+++ DSA/Array_1/input.cpp
@@ -0,0 +1,46 @@
+// Taking Input in Array.
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int arr[5];
+//     int n = 5;
+
+//     // Taking Input In Array
+//     for(int i = 0; i < n; i++){
+//         cout << "Enter the value of index " << i << ": " ;
+//         cin >> arr[i];
+//         cout << endl;
+       
+//     }
+
+//     // Printing An Array
+//     cout << "Printing the array" << endl;
+
+//     for(int i = 0; i< n; i++ ){
+//         cout << arr[i] << " " ;
+//     }
+// }
+
+
+
+// Double the each value of an array.
+#include <iostream>
+using namespace std; 
+
+int main() {
+
+    int arr[10];
+    int n = 5;
+    for(int i = 0; i < n; i++){
+        cout << "Enter the value of index " << i << ": " ;
+        cin >> arr[i];
+    }
+    cout << "Printing the Araay in double of my input:" << endl;
+    for(int i = 0; i < n ; i++){
+        arr[i] = 2 * arr[i];
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
